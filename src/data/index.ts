1734d30d2c7ea8edd1837bd829c8d62f76de6d3f
nahuelfaria <nahuellucianofaria2001@outlook.com>
2024-07-04 11:57:38 -0300

    first commit

---
+++ src/data/index.ts
@@ -0,0 +1,10 @@
+
+
+
+export * from './mongo/mongo-database';
+
+export * from './mongo/models/user.model';
+
+export * from './mongo/models/product.model';
+
+export * from './mongo/models/category.model';
